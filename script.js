cf1b387edf632dc7ded4a9ce557eb8f0b8f25597
rajivcgit <143603198+rajivcgit@users.noreply.github.com>
2023-08-30 22:32:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const imageContainerEl = document.querySelector('.image-container');
+const btnEl = document.querySelector('.btn');
+
+btnEl.addEventListener('click', () => {
+    imgNum = 8;
+    addNewImages();
+});
+
+
+function addNewImages() {
+    for (let index = 0; index < imgNum; index++) {
+        const newImgEl = document.createElement('img');
+        newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+        imageContainerEl.appendChild(newImgEl);
+    }
+}
